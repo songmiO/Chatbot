3d8dc38b8edd1d2d8c217f7d0c0ba61d57a45329
songmiO <dhthd1060@gmail.com>
2021-10-20 22:47:52 +0900

    Add : Add a style color

--- miniChatBot.js
+++ miniChatBot.js
@@ -3,18 +3,12 @@ const movieFirst = {
   openingDate: '1996년 3월 16일',
 };
 
-const movieSecond = {
-  title: 'Inception',
-  openingDate: '2010년 7월 21일',
-};
-
 const title = document.getElementById('title');
 const openingDate = document.getElementById('openingDate');
-const genre = document.getElementById('genre');
 
 function openMovie(movie) {
-  console.log('출력');
   title.innerText = movie.title;
   openingDate.innerText = movie.openingDate;
   genre.innerText = movie.genre;
+  console.log('출력');
 }

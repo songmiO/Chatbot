168b08e69fb54ec5a441c5a287aa8ffec3a71e42
songmiO <dhthd1060@gmail.com>
2021-10-23 23:51:12 +0900

    Modify : Modifying the code

--- miniChatBot.js
+++ miniChatBot.js
@@ -2,6 +2,3 @@ const movieFirst = {
   title: 'Before Sunrise',
   openingDate: '1998년 3월 16일',
 };
-
-const title = document.getElementById('title');
-const openingDate = document.getElementById('openingDate');

5f3e3163a6b41a146d0ca99e33c7500a1dc0149b
songmiO <dhthd1060@gmail.com>
2021-10-26 22:49:13 +0900

    Add : Add the opening date

--- miniChatBot.js
+++ miniChatBot.js
@@ -1,5 +1,7 @@
 const movieFirst = {
   title: 'Midnight Sun',
+  opningdate: '2018.03.22',
 };
 
 const title = document.getElementById('title');
+const opningdate = document.getElementById('openingDate');

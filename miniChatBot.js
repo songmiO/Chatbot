ec7116ae1461318fb81b4b8753c48839991711cb
songmiO <dhthd1060@gmail.com>
2021-11-03 22:10:09 +0900

    Modify : Change the color.

--- miniChatBot.js
+++ miniChatBot.js
@@ -1,7 +1,7 @@
 function changeSpeak() {
   const inputValue = document.getElementById('input').ariaValueMax;
 
-  if (inputValue == order.blue) {
+  if (inputValue == order.red) {
     document.body.style.backgroundColor = 'red';
   } else if (inputValue == order.white) {
     document.body.style.backgroundColor = 'white';

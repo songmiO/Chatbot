1eb96168cf1a8a6522ebdf6eca73c5ff38a803d1
songmiO <dhthd1060@gmail.com>
2021-12-12 23:00:35 +0900

    Modify : Modify the code.

--- miniChatBot.js
+++ miniChatBot.js
@@ -1,14 +1,55 @@
-function changeSpeak() {
-  const inputValue = document.getElementById('input').ariaValueMax;
+var json = [
+  {
+    question: '반가워!',
+    answer: '사랑해!',
+  },
+];
 
-  if (inputValue == order.blue) {
-    document.body.style.backgroundColor = 'blue';
-  } else if (inputValue == order.white) {
-    document.body.style.backgroundColor = 'white';
-  } else {
-    const chatting = document.getElementById('chatBot');
-    const writing = new Writer(chatting, { loop: false });
+var question = '';
+var answer = '';
+
+var key = 0;
+
+function btnClick() {
+  var value = document.getElementById('input').value;
+
+  if (key == 1) {
+    if (value == '네') {
+      reply.innerHTML = '당신의 답변을 입력하세요.';
+      key = 2;
+    } else {
+      reply.innerHTML = '명령어를 입력해주세여.';
+      key = 0;
+    }
+    return;
   }
-  document.getElementById('input').value = '';
-  console.log('확인');
+
+  if (key == 2) {
+    answer = value;
+    push_json();
+  }
+
+  function push_json() {
+    json.push({ question: `${question}`, answer: `${answer}` });
+    document.getElementById('txt-out').innerHTML = '명령어 저장!';
+    key = 0;
+  }
+
+  for (let i = 0; i < json.length; i++) {
+    if (value == json[i].question) {
+      reply.innerHTML = json[i].answer;
+      return;
+    }
+  }
+
+  reply.innerHTML = '명령어를 입력해줘!';
+  question = value;
+  key = 1;
+
+  for (let i = 0; i < json.length; i++) {
+    if (value == json[i].question) {
+      reply.innerHTML = json[i].answer;
+      return;
+    }
+  }
 }

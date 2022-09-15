7ee7a1e9c5afbfab2dd9a79aad971194f80edb31
songmiO <dhthd1060@gmail.com>
2022-09-15 21:27:34 +0900

    Add : Add data

--- chatBot/index.js
+++ chatBot/index.js
@@ -1,2 +1,8 @@
 // input창에 안녕 입력, 전송 버튼 누르면 -> 반가워 라고 대답하기
-document.getElementsByClassName();
+const data = [
+  { chat: '안녕', answer: '반가워' },
+  { chat: '오늘 어때?', answer: '기분 좋아' },
+  { chat: '사랑해', answer: '나두 사랑해❤️' },
+  { chat: '오늘 기분 어때?', answer: '최고야!' },
+  { chat: '너는 누구야?', answer: '난 귀여운 피카츄야!' },
+];

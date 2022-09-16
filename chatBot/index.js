18ab5d218aa04b55bc16378802a5d94e2fdfe8c3
songmiO <dhthd1060@gmail.com>
2022-09-16 21:57:22 +0900

    Refactor : Refactor index code

--- chatBot/index.js
+++ chatBot/index.js
@@ -1,8 +1,24 @@
-// input창에 안녕 입력, 전송 버튼 누르면 -> 반가워 라고 대답하기
-const data = [
-  { chat: '안녕', answer: '반가워' },
-  { chat: '오늘 어때?', answer: '기분 좋아' },
-  { chat: '사랑해', answer: '나두 사랑해❤️' },
-  { chat: '오늘 기분 어때?', answer: '최고야!' },
-  { chat: '너는 누구야?', answer: '난 귀여운 피카츄야!' },
-];
+// input창에 안녕 입력, 전송 버튼 누르면 -> 박스 속 글자가 바뀐다.
+
+function confirm() {
+  const inputValue = document.getElementsByClassName('chat-input')[0].value;
+  const chatBotBox = document.getElementsByClassName('chatbot-box')[0];
+
+  if (inputValue == '안녕') {
+    chatBotBox.innerHTML = '반가워!';
+  } else if (inputValue == '너 이름이 뭐야?') {
+    chatBotBox.innerHTML = '난 전기 포켓몬 피카츄야!';
+  } else if (inputValue == '백만볼트') {
+    document.getElementsByClassName(
+      'chatbot-change-image'
+    )[0].style.visibility = 'visible';
+    // 글자와 그림 동시에 나타나게 하고 싶은데 안됨!
+    // document.getElementsByClassName('chatbot-box')[0].innerHTML = '피카~츄!';
+  } else if (inputValue == '오늘 기분 어때?') {
+    chatBotBox.innerHTML = '아주 좋아!😊';
+  } else if (inputValue == '사랑해') {
+    chatBotBox.innerHTML = '내가 더 ❤️❤️❤️';
+  } else {
+    chatBotBox.innerHTML = '아직 배우지 않은 말이예요. 가르쳐 주세요!';
+  }
+}
